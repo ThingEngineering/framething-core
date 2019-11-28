a18e96ce6bf93d5296a6b436a6f0a36ca4045343
Freddie <freddie@wafflemonster.org>
2019-11-28 14:08:36 -0800

    Fix wonky namespace

--- src/Framething.Web/Program.cs
+++ src/Framething.Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace framething
+namespace Framething.Web
 {
     public class Program
     {
--- src/Framething.Web/Startup.cs
+++ src/Framething.Web/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace framething
+namespace Framething.Web
 {
     public class Startup
     {
