bcbfb0659a11d359316b78e128f05b429a61b992
Freddie <freddie@wafflemonster.org>
2019-11-30 12:55:09 -0800

    Add CSS/JS generation garbage

---
+++ src/Framething.Web/webpack.config.js
@@ -0,0 +1,51 @@
+﻿const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const mode = argv.mode === 'production' ? 'production' : 'development';
+    return {
+        mode: mode,
+        entry: {
+            framething: [
+                './Static/JS/framething.js',
+                './Static/CSS/framething.scss',
+            ],
+        },
+        output: {
+            filename: mode === 'production' ? '[name].[chunkhash].js' : '[name].js',
+            path: path.resolve(__dirname, 'wwwroot/dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/,
+                    use:
+                        [
+                            //'style-loader',
+                            MiniCssExtractPlugin.loader,
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    config: {
+                                        ctx: {
+                                            env: mode
+                                        }
+                                    }
+                                }
+                            },
+                            'sass-loader'
+                        ]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: mode === 'production' ? '[name].[contenthash].css' : '[name].css',
+            }),
+        ]
+    };
+};
